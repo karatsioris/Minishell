109412ef3c09d49dfe8d95dfdcf7df92afb050bf
Florian Hensel <florianhensel@proton.me>
2026-06-21 16:01:33 +0200

    fix: improve cd command argument validation and update PWD/OLDPWD environment variables

--- src/executer/builtin_cd_exit.c
+++ src/executer/builtin_cd_exit.c
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <limits.h>
 
 #include "builtin_internal.h"
 #include "libft.h"
@@ -27,29 +28,43 @@ static void	print_cd_error(const char *path)
 	ft_putstr_fd("\n", STDERR_FILENO);
 }
 
+static int	cd_arg_error(t_node *node, t_shell *shell)
+{
+	if (!node->args[1])
+		ft_putstr_fd("minishell: cd: path required\n", STDERR_FILENO);
+	else if (node->args[2])
+		ft_putstr_fd("minishell: cd: too many arguments\n", STDERR_FILENO);
+	else
+		return (0);
+	shell->exit_code = 1;
+	return (1);
+}
+
+static void	update_pwd_env(t_shell *shell, char *oldcwd)
+{
+	char	newcwd[PATH_MAX];
+
+	if (oldcwd[0])
+		set_env_value(shell, "OLDPWD", oldcwd);
+	if (getcwd(newcwd, sizeof(newcwd)))
+		set_env_value(shell, "PWD", newcwd);
+}
+
 int	builtin_cd(t_node *node, t_shell *shell)
 {
-	const char	*path;
+	char	oldcwd[PATH_MAX];
 
-	if (!node->args[1])
+	if (cd_arg_error(node, shell))
+		return (1);
+	if (!getcwd(oldcwd, sizeof(oldcwd)))
+		oldcwd[0] = '\0';
+	if (chdir(node->args[1]) != 0)
 	{
-		ft_putstr_fd("minishell: cd: path required\n", STDERR_FILENO);
+		print_cd_error(node->args[1]);
 		shell->exit_code = 1;
 		return (1);
 	}
-	if (node->args[2])
-	{
-		ft_putstr_fd("minishell: cd: too many arguments\n", STDERR_FILENO);
-		shell->exit_code = 1;
-		return (1);
-	}
-	path = node->args[1];
-	if (chdir(path) != 0)
-	{
-		print_cd_error(path);
-		shell->exit_code = 1;
-		return (1);
-	}
+	update_pwd_env(shell, oldcwd);
 	shell->exit_code = 0;
 	return (0);
 }
--- src/runtime/main_utils.c
+++ src/runtime/main_utils.c
@@ -14,7 +14,38 @@
 #include "token.h"
 #include "parse.h"
 #include "builtin.h"
+#include "builtin_internal.h"
+#include "libft.h"
 #include <unistd.h>
+
+static char	*get_env_val(char **envp, const char *name)
+{
+	int	index;
+
+	index = env_find_index(envp, name);
+	if (index < 0)
+		return (NULL);
+	return (ft_strchr(envp[index], '=') + 1);
+}
+
+static void	increment_shlvl(t_shell *shell)
+{
+	char	*current;
+	char	*value;
+	int		level;
+
+	current = get_env_val(shell->envp, "SHLVL");
+	level = 1;
+	if (current)
+		level = ft_atoi(current) + 1;
+	if (level < 0)
+		level = 0;
+	value = ft_itoa(level);
+	if (!value)
+		return ;
+	set_env_value(shell, "SHLVL", value);
+	free(value);
+}
 
 void	initialization(t_shell	*shell, char **envp)
 {
@@ -23,6 +54,8 @@ void	initialization(t_shell	*shell, char **envp)
 	shell->envp = dup_envp(envp);
 	shell->line = NULL;
 	shell->interactive = isatty(STDIN_FILENO);
+	if (shell->envp)
+		increment_shlvl(shell);
 }
 
 void	cleanup(t_shell *shell, t_token	**all_token,
