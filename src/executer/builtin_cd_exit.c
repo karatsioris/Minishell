068d95e3da2ce847320a2f685893bb7654639e49
Florian Hensel <florianhensel@proton.me>
2026-06-21 14:54:40 +0200

    fix: reorder exit argument validation

--- src/executer/builtin_cd_exit.c
+++ src/executer/builtin_cd_exit.c
@@ -56,12 +56,6 @@ int	builtin_cd(t_node *node, t_shell *shell)
 
 int	builtin_exit(t_node *node, t_shell *shell)
 {
-	if (node->args[1] && node->args[2])
-	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
-		shell->exit_code = 1;
-		return (1);
-	}
 	if (node->args[1] && !is_valid_exit_arg(node->args[1]))
 	{
 		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
@@ -70,6 +64,12 @@ int	builtin_exit(t_node *node, t_shell *shell)
 		shell->exit_code = 2;
 		shell->running = 0;
 		return (shell->exit_code);
+	}
+	if (node->args[1] && node->args[2])
+	{
+		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+		shell->exit_code = 1;
+		return (1);
 	}
 	if (node->args[1])
 		shell->exit_code = ft_atoi(node->args[1]);
