f8284a1195af3c2688181c9e81e00ea690e8e72b
Florian Hensel <florianhensel@proton.me>
2026-06-21 11:49:53 +0200

    fix: updated error handling so it is matcheing bash

--- src/executer/builtin_cd_exit.c
+++ src/executer/builtin_cd_exit.c
@@ -12,9 +12,20 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 #include "builtin_internal.h"
 #include "libft.h"
+
+static void	print_cd_error(const char *path)
+{
+	ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+	ft_putstr_fd((char *)path, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(strerror(errno), STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+}
 
 int	builtin_cd(t_node *node, t_shell *shell)
 {
@@ -35,7 +46,7 @@ int	builtin_cd(t_node *node, t_shell *shell)
 	path = node->args[1];
 	if (chdir(path) != 0)
 	{
-		perror("minishell: cd");
+		print_cd_error(path);
 		shell->exit_code = 1;
 		return (1);
 	}
